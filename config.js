d9923f149162b2063cdc4bb00057e442e192afc4
Asim Ali Murtaza <mo.asim.murtaza@gmail.com>
2024-11-15 10:12:38 +0500

    update

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-const secretKey = process.env.MY_SECRET_KEY; console.log("Your Secret Key is:", secretKey);
+const secretKey = process.env.MY_SECRET_KEY;
+console.log("Your Secret Key is:", secretKey);
